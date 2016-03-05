bcb08cb4da1484d46b7220592332811a99c42b3d
Jia Jia Geng <gjj0816@gmail.com>
2016-03-05 15:42:27 +0800

    Update generate_function.py
    add the valid_model

--- generate_function.py
+++ generate_function.py
@@ -1,23 +1,18 @@
 import theano
 
 class function:
-    def __init__(self, input, outputs, updates, givens):
+    def __init__(self, input, outputs, prediction, updates, givens):
         self.train_model = theano.function(
             inputs=input,
             outputs=outputs,
             updates=updates,
             givens=givens[0]
         )
-        '''
+
         self.valid_model = theano.function(
             inputs=input,
             outputs=outputs,
             givens=givens[1]
         )
-        '''
-        self.test_model = theano.function(
-            inputs=input,
-            outputs=outputs,
-            givens=givens[1]
-        )
-        self.model = [self.train_model, self.test_model]
+
+        self.model = [self.train_model, self.valid_model]
