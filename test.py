871b024ff8badc4e5905eba51ab2f4c60974fe0e
sai <gjj0816@gmail.com>
2016-03-08 09:11:36 +0800

    测试 conv_3x3 method

---
+++ test.py
@@ -0,0 +1,20 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+import Layers
+
+rng = np.random.RandomState(1234)
+image = cv2.imread('./dataset/images/1.jpg', 0)
+input = image.reshape(1, 1, image.shape[0], image.shape[1])
+input = np.array(input, dtype=np.float64)
+conv_3x3 = Layers.conv_3x3(input, rng, input.shape, (4, 1, 3, 3), (6, 4, 3, 3))
+out = conv_3x3.outputs
+value = out.eval()
+print value.shape
+
+for i in xrange(value.shape[1]):
+    plt.subplot(2, 3, i + 1)
+    plt.imshow(value[0, i, :, :], 'gray')
+
+plt.show()
